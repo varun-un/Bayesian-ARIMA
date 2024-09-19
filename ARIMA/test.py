8d64fcf6cfa9636aa79a74dcdb93e7b4dffd21b3
Varun Unnithan <varun.unnithan33@gmail.com>
2024-09-19 15:19:19 -0400

    ACF functions for order

--- ARIMA/test.py
+++ ARIMA/test.py
@@ -1,22 +1,20 @@
-# Import necessary libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
-# Stock price data you provided
+# Sample stock price data
 data = {
     'Time': [0, 1, 2, 3, 4, 5, 6, 7, 8],
     'Price': [50, 56, 59, 60, 56, 52, 59, 63, 64]
 }
 
-# Convert data to pandas DataFrame
 df = pd.DataFrame(data)
 
-# Set 'Time' as index to treat it as a time series
+# set 'Time' as index to treat it as a time series
 df.set_index('Time', inplace=True)
 
-# Plot the raw data
+# plot raw data
 plt.figure(figsize=(10, 5))
 plt.plot(df, marker='o')
 plt.title('Stock Prices Over Time')
@@ -24,14 +22,28 @@ plt.xlabel('Time')
 plt.ylabel('Price')
 plt.show()
 
-# First order differencing to remove any trend
+# 1st order differencing to remove any trend
 df_diff = df.diff().dropna()
 
-# Plot the differenced data
 plt.figure(figsize=(10, 5))
 plt.plot(df_diff, marker='o', color='green')
 plt.title('Differenced Stock Prices (First Order)')
 plt.xlabel('Time')
 plt.ylabel('Differenced Price')
+plt.show()
+
+
+# ACF and PACF for the differenced data
+plt.figure(figsize=(12, 5))
+
+plt.subplot(121)
+plot_acf(df_diff, ax=plt.gca(), lags=7)
+plt.title('ACF of Differenced Data')
+
+plt.subplot(122)
+plot_pacf(df_diff, ax=plt.gca(), lags=4)
+plt.title('PACF of Differenced Data')
+
+plt.tight_layout()
 plt.show()
 
