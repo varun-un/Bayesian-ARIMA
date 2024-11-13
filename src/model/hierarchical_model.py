9d5fae878212554078bc778b27ff8a3645bea6b0
Varun Unnithan <varun.unnithan33@gmail.com>
2024-11-13 16:25:27 -0500

    Full hierarchical model

--- src/model/hierarchical_model.py
+++ src/model/hierarchical_model.py
@@ -1,4 +1,5 @@
 from bayesian_arima import BayesianARIMA
+from model_selection import determine_arima_order
 import pandas as pd
 from typing import Dict
 
@@ -29,4 +30,62 @@ class HierarchicalModel:
             'hourly': None,
             'minute': None
         }
+        self.seasonality = {
+            'monthly': 12,
+            'daily': 5,
+            'hourly': 6,
+            'minute': 1
+        }
 
+    def add_model(self, timeframe: str, model: BayesianARIMA):
+        """
+        Add a Bayesian ARIMA model for a specific timeframe. (usually an internal method)
+        """
+        if timeframe not in self.models:
+            raise ValueError("Invalid timeframe. Choose from 'monthly', 'daily', 'hourly', 'minute'.")
+        self.models[timeframe] = model
+
+    def train_all(self, data: Dict[str, pd.Series], exog: Dict[str, pd.Series], sampler):
+        """
+        Train all ARIMA models using the provided data and sampler. The exogenous variables are optional.
+        This will also create and add models for all time intervals.
+
+        Parameters:
+        - data: Dictionary of timeframes to series. The keys should be 'monthly', 'daily', 'hourly', 
+                'minute', and the values should be pandas Series.
+        - exog: Dictionary of timeframes to exogenous variables. The keys should be 'monthly', 'daily', 
+                'hourly', 'minute', and the values should be pandas Series. If no exogenous variables, 
+                set to `None`.
+        - sampler: A sampler object to provide training data
+        """
+        for timeframe, series in data.items():
+            sampled_series = sampler.sample(series)
+            if exog is None:
+                sampled_exog = None
+            else:
+                sampled_exog = exog.get(timeframe, None)
+
+            # get seasonality and ARIMA order
+            seasonality = self.seasonality[timeframe]
+            p, d, q = determine_arima_order(sampled_series, seasonal=(seasonality > 1), m=seasonality)
+            
+            # create and train the models
+            model = BayesianARIMA(p, d, q)
+            model.train(sampled_series, sampled_exog)
+            self.add_model(timeframe, model)
+
+    def predict_all(self, steps: int) -> Dict[str, pd.Series]:
+        """
+        Generate forecasts from all models. Forward passes through the ARIMA models
+
+        Parameters:
+        - steps: Number of steps to forecast into the future.
+
+        Returns:
+        - dict: Dictionary of timeframes to forecasted series. Keys are 'monthly', 'daily', 'hourly', 'minute'.
+                Values are pandas series of forecasted values and length `steps`.
+        """
+        forecasts = {}
+        for timeframe, model in self.models.items():
+            forecasts[timeframe] = model.predict(steps)
+        return forecasts
